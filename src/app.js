1f721eadc9067ce30196f2ad6ec91cafe5a7a4a4
Vaibhav Raj <40136017+vaibhav-raj@users.noreply.github.com>
2024-05-01 10:09:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorHandler = require('../src/middleware/errorHandler');
 const Auth = require('./middleware/auth');
+const fs = require('fs');
 const app = express();
 
 // Middleware
@@ -18,8 +19,10 @@ const tvShow = require('./routes/tvShowRoutes');
 const myList = require('./routes/myListRoutes');
 
 
+// Read the JSON file
+const data = JSON.parse(fs.readFileSync('bio.json', 'utf-8'));
 app.get('/api/owner', (req, res) => {
-    res.json({ "name": "vaibhav" });
+    res.json(data);
 });
 
 app.use('/api/users', user);
